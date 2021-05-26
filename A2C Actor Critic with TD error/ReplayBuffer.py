f69de21b39d8fdb503308218183d5c8c079057db
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2021-05-26 14:45:03 -0400

    Update ReplayBuffer.py

--- A2C Actor Critic with TD error/ReplayBuffer.py
+++ A2C Actor Critic with TD error/ReplayBuffer.py
@@ -6,15 +6,17 @@ class ReplayBuffer():
         self.mem_cntr         = 0
         self.state_memory     = np.zeros((self.mem_size, *input_shape),dtype=np.float32)
         self.new_state_memory = np.zeros((self.mem_size, *input_shape),dtype=np.float32)
+        self.actions          = np.zeros(self.mem_size, dtype=np.float32)
         self.log_probs        = np.zeros(self.mem_size, dtype=np.float32)
         self.reward_memory    = np.zeros(self.mem_size, dtype=np.float32)
         self.terminal_memory  = np.zeros(self.mem_size, dtype=np.uint8)
 
-    def store_transition(self, state, log_prob, reward, state_, done):
+    def store_transition(self, state, action, log_prob, reward, state_, done):
         index                       = self.mem_cntr % self.mem_size
         
         self.state_memory[index]     = state
         self.new_state_memory[index] = state_
+        self.actions[index]          = action
         self.log_probs[index]        = log_prob
         self.reward_memory[index]    = reward
         self.terminal_memory[index]  = done
@@ -26,9 +28,10 @@ class ReplayBuffer():
         batch   = np.random.choice(max_mem, batch_size, replace=False)
 
         states   = self.state_memory[batch]
+        actions  = self.actions[batch]
         probs    = self.log_probs[batch]
         rewards  = self.reward_memory[batch]
         states_  = self.new_state_memory[batch]
         terminal = self.terminal_memory[batch]
 
-        return states, probs, rewards, states_, terminal
+        return states, actions, probs, rewards, states_, terminal
