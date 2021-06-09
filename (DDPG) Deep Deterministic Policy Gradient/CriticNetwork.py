b0fb76647646f64a269f633348f4253357da4a62
Abdelhamid bouzid <abdlhamidbouzid@gmail.com>
2021-06-08 23:05:03 -0400

    Add files via upload

---
+++ (DDPG) Deep Deterministic Policy Gradient/CriticNetwork.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 15:51:30 2021
+
+@author: Admin
+"""
+import os
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+class CriticNetwork(nn.Module):
+    def __init__(self, beta, input_dims, fc1_dims, fc2_dims, n_actions, name,chkpt_dir='tmp/ddpg'):
+        super(CriticNetwork, self).__init__()
+        
+        self.input_dims      = input_dims
+        self.fc1_dims        = fc1_dims
+        self.fc2_dims        = fc2_dims
+        self.n_actions       = n_actions
+        self.checkpoint_file = os.path.join(chkpt_dir,name+'_ddpg')
+        self.fc1             = nn.Linear(*self.input_dims, self.fc1_dims)
+        
+        f1 = 1./np.sqrt(self.fc1.weight.data.size()[0])
+        T.nn.init.uniform_(self.fc1.weight.data, -f1, f1)
+        T.nn.init.uniform_(self.fc1.bias.data, -f1, f1)
+        #self.fc1.weight.data.uniform_(-f1, f1)
+        #self.fc1.bias.data.uniform_(-f1, f1)
+        self.bn1 = nn.LayerNorm(self.fc1_dims)
+
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        f2       = 1./np.sqrt(self.fc2.weight.data.size()[0])
+        #f2 = 0.002
+        T.nn.init.uniform_(self.fc2.weight.data, -f2, f2)
+        T.nn.init.uniform_(self.fc2.bias.data, -f2, f2)
+        #self.fc2.weight.data.uniform_(-f2, f2)
+        #self.fc2.bias.data.uniform_(-f2, f2)
+        self.bn2 = nn.LayerNorm(self.fc2_dims)
+
+        self.action_value = nn.Linear(self.n_actions, self.fc2_dims)
+        f3 = 0.003
+        self.q = nn.Linear(self.fc2_dims, 1)
+        T.nn.init.uniform_(self.q.weight.data, -f3, f3)
+        T.nn.init.uniform_(self.q.bias.data, -f3, f3)
+        #self.q.weight.data.uniform_(-f3, f3)
+        #self.q.bias.data.uniform_(-f3, f3)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cuda:1')
+
+        self.to(self.device)
+
+    def forward(self, state, action):
+        state_value = self.fc1(state)
+        state_value = self.bn1(state_value)
+        state_value = F.relu(state_value)
+        state_value = self.fc2(state_value)
+        state_value = self.bn2(state_value)
+
+        action_value       = F.relu(self.action_value(action))
+        state_action_value = F.relu(T.add(state_value, action_value))
+        state_action_value = self.q(state_action_value)
+
+        return state_action_value
+
+    def save_checkpoint(self):
+        print('... saving checkpoint ...')
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        print('... loading checkpoint ...')
+        self.load_state_dict(T.load(self.checkpoint_file))
